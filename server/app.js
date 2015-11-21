d8f4882b9112f9e57017d9930f4e655072b8c762
rsamokhin <poshliemail@googlemail.com>
2015-11-22 02:03:34 +0300

    server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-var koa = require('koa'),
+var config = require('./config'),
+    koa = require('koa'),
     app = koa(),
     route = require('koa-route'),
     serve = require('koa-static');
 
 app.use(serve('../web/build/'));
 
-app.listen(3000);
+app.listen(config.get('port'));
 
 console.log('static served');
