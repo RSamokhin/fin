fe96cab4754402e5bafe40116fdc16fbd17dad5d
rsamokhin <poshliemail@googlemail.com>
2015-11-22 02:13:02 +0300

    forever installed

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var forever = require('forever-monitor');
+var fs = require('fs');
+var watchIgnorePatterns = [
+    'server/db/**',
+    'node_modules/**',
+    '.git/**'
+];
+
+var child = new(forever.Monitor)('./app.js', {
+    'silent': process.env.NODE_ENV !== 'develop',
+    'pidFile': 'server/pids/app.pid',
+    'watch': true,
+    'watchDirectory': './',
+    'watchIgnoreDotFiles': true,
+    'watchIgnorePatterns': watchIgnorePatterns,
+    'logFile': fs.existsSync('logs/forever.log') ? 'logs/forever.log' : '',
+    'outFile': fs.existsSync('logs/forever.out') ? 'logs/forever.out' : '',
+    'errFile': fs.existsSync('logs/forever.err') ? 'logs/forever.err' : ''
+});
+
+child.on('watch:restart', function(info) {
+    console.error('Restaring script because ' + info.stat + ' changed');
+});
+
+child.start();
