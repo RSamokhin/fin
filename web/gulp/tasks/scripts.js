8b90934ed67d18e44a6943bb34d16a5d8ce28628
rsamokhin <poshliemail@googlemail.com>
2015-11-22 21:03:25 +0300

    gulp clean tested

--- web/gulp/tasks/scripts.js
+++ web/gulp/tasks/scripts.js
@@ -1,26 +1,12 @@
-var gulp = require('gulp');
-    gulpif = require('gulp-if');
-    plumber = require('gulp-plumber');
-    webpackStream = require('webpack-stream');
-    browserSync = require('browser-sync');
+var gulp = require('gulp'),
+    gulpif = require('gulp-if'),
+    plumber = require('gulp-plumber'),
+    connect = require('gulp-connect'),
     path = require('../config.js').path;
-    webpackConfig = require('../../webpack.config.js');
-    errorHandler = require('../errorHandler.js');
-
-gulp.task('scripts', function scriptsTask() {
-    return gulp.src(path.src.js)
-        .pipe(plumber({errorHandler: errorHandler}))
-        .pipe(webpackStream(webpackConfig))
-        .pipe(gulp.dest(path.build.js))
-        .pipe(gulpif(global.watch, browserSync.stream()));
-});
-
-
 
 gulp.task('scripts',function scriptsTask () {
     return gulp.src(path.src.js)
         .pipe(plumber())
-        .pipe(rigger())
         .pipe(gulp.dest(path.build.js))
-        .pipe(connect.reload());
+        .pipe(gulpif(global.watch, connect.reload()));
 });
