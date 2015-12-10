095df929429e3f63ed6fa02fff8b53c51b206133
rSamokhin <poshliemail@googlemail.com>
2015-12-11 01:04:36 +0300

    all forms collapsed in one

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,19 +1,20 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 170){
-        $('.main__header-splitter').addClass("m-sticky");
-    }
-    else{
-        $('.main__header-splitter').removeClass("m-sticky");
-    }
-});
-
 window.Handlers = {
     click: {
-        showToggledForm: function()
-        {
-            $('.toggled-clients-block-form').hide();
-            var formName = $(this).data('formName');
-            $('.' + formName).show();
+        showToggledForm: function() {
+            var $button = $(this),
+                fName = $button.attr('data-form-name'),
+                $parent = $button.closest('[data-main-block=true]');
+            $parent.find('[data-form][data-form!=' + fName + ']').addClass('m-hidden');
+            $parent.find('[data-form=' + fName + ']').toggleClass('m-hidden');
+        },
+        blockFormCollapse: function () {
+            $(this).closest('[data-form]').addClass('m-hidden');
+        },
+        blockCollapseToggle: function () {
+           $(this).closest('[data-main-block=true]').toggleClass('m-collapsed');
+        },
+        blockToogleFavs: function () {
+            $(this).closest('[data-main-block=true]').find('[data-block-fav=true]').toggleClass('m-active');
         }
     }
 };
@@ -25,4 +26,13 @@ $(function(){
             $(document.body).on(bindFunctionEvent, '[data-bind-'+bindFunctionEvent+'*='+bindFunctionName+']', window.Handlers[bindFunctionEvent][bindFunctionName]);
         });
     });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 170){
+        $('.main__header-splitter').addClass("m-sticky");
+    }
+    else{
+        $('.main__header-splitter').removeClass("m-sticky");
+    }
 });
